c88420eb01a38d046640246d8a3ad1ce45e6ec97
omar15623 <omarahel98@gmail.com>
2025-12-06 22:34:23 +0200

    Add the actual code content

---
+++ Project_Click_Counter.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+counter = 0
+
+def increment_counter():
+    global counter
+    counter += 1
+    mylabel.config(text=f"عدد النقرات الآن: {counter}")
+
+myframe = Tk()
+myframe.title("عداد النقرات (Click Counter)")
+myframe.geometry("350x200")
+
+mylabel = Label(
+    myframe, 
+    text=f"عدد النقرات الآن: {counter}", 
+    font="Helvatica 18 bold", 
+    fg="blue"
+)
+mylabel.pack(pady=30) 
+
+mybutton = Button(
+    myframe,
+    text="اضغط لزيادة العداد",
+    fg="white", 
+    bg="#008CBA",
+    font="Helvatica 12 bold",
+    padx=15, 
+    pady=8,
+    command=increment_counter,
+)
+mybutton.pack(pady=10)
+
+myframe.mainloop()
